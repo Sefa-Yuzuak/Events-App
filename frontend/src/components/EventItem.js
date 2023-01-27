366e2e2caa821bc07d95ff00ded6d6d832d80478
SefaYuzuak <112412065+Sefa-Yuzuak@users.noreply.github.com>
2023-01-27 08:53:45 +0300

    Add files via upload

---
+++ frontend/src/components/EventItem.js
@@ -0,0 +1,32 @@
+import { Form, Link, useSubmit } from "react-router-dom";
+
+import classes from './EventItem.module.css';
+
+function EventItem({ event }) {
+  const submit = useSubmit();
+
+  function startDeleteHandler() {
+    const proceed = window.confirm('Are you sure?');
+
+    if(proceed){
+      submit(null, {method: 'delete'})
+    }
+  }
+
+  return (
+    <article className={classes.event}>
+      <img src={event.image} alt={event.title} />
+      <h1>{event.title}</h1>
+      <time>{event.date}</time>
+      <p>{event.description}</p>
+      <menu className={classes.actions}>
+        <Link to="edit">Edit</Link>
+        <Form>
+          <button onClick={startDeleteHandler}>Delete</button>
+        </Form>
+      </menu>
+    </article>
+  );
+}
+
+export default EventItem;
